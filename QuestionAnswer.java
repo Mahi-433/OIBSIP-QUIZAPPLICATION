d53e7a5fd4010550e1144bcd855e2a321ce2e6dc
Mahi-433 <133677337+Mahi-433@users.noreply.github.com>
2023-07-12 20:47:15 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,58 @@
+package com.example.quizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "which company owns the android?",
+            "which one is not the programming language?",
+            "who is known as Iron man of India?",
+            "Entomology is the science that studies?",
+            "Who is the father of Geometry?",
+            "The nucleus of an atom consists of?",
+            "The percentage of irrigated land in India is about?",
+            "The percentage of earth surface covered by India is?",
+            "Which scientist discovered the radioactive element radium?",
+            "The Indian Institute of Science is located at?",
+            "What is part of a database that holds only one type of information?",
+            "Who is the father of English Poetry?",
+            "The ratio of width of our National flag to its length is?",
+            "Who is the first Indian woman to win an Asian Games gold in 400m run?",
+            "Which of the following is not associated with the UNO?"
+    };
+
+    public static String choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Govind Ballabh Pant","Jawaharlal Nehru","Subhash Chandra Bose","Sardar Vallabhai Patel"},
+            {"Behavior of human beings","Insects","The origin and history of technical and scientific terms","The formation of rocks"},
+            {"Aristotle","Euclid","Pythagoras","Kepler"},
+            {"electrons and neutrons","electrons and protons","protons and neutrons","All of the above"},
+            {"45","65","35","25"},
+            {"2.4","3.4","4.4","5.4"},
+            {"Isaac Newton","Albert Einstein","Benjamin Franklin","Marie Curie"},
+            {"Kerala","Madras","Banglore","New Delhi"},
+            {"Report","Field","Record","File"},
+            {"Milton","Wordsworth","Chaucer","Charles Dickens"},
+            {"3:5","2:3","2:4","3:4"},
+            {"M.L.Valsamma","P.T.Usha","Kamaljit Sandhu","K.Malleshwari"},
+            {"ILO","WHO","ASEAN","All of the above"}
+    };
+
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "Sardar Vallabhai Patel",
+            "Insects",
+            "Euclid",
+            "protons and neutrons",
+            "35",
+            "2.4",
+            "Marie Curie",
+            "Banglore",
+            "Field",
+            "Chaucer",
+            "2:3",
+            "Kamaljit Sandhu",
+            "ASEAN"
+    };
+}
